2f497660265f646b827718ed39d3b5a390d773f5
Rudenko Artem <artem.rud@bk.ru>
2019-10-31 16:00:19 +0200

    Updates

---
+++ precache-manifest.3579a18e528cfa631cff87669946365e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33ba1cbafb2ad46dfd5cab81b5375bf",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "4abb9056a9087a567416",
+    "url": "/react_tabs/static/css/main.f8e06b97.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/react_tabs/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "4abb9056a9087a567416",
+    "url": "/react_tabs/static/js/main.fae9085e.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
